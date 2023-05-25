4408b889c51097b23b2f198ddef49bc1b55f360e
ankitt0786 <ankitsingh7295@gmail.com>
2023-05-25 20:28:46 +0530

    Add project files.

---
+++ WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        //private readonly IBookRepository _bookRepository2;
+
+        /// <summary>
+        /// Constructor Injection
+        /// </summary>
+        /// <param name="bookRepository"></param>
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+           // _bookRepository2 = bookRepository2;
+        }
+
+        [HttpPost("")]
+        public IActionResult AddBook([FromBody] BookModel book)
+        {
+            int id = _bookRepository.AddBook(book);
+            //var books = _bookRepository2.GetAllBooks();
+            return Ok(id);
+        }
+
+        [HttpGet("")]
+        public IActionResult GetAllBooks()
+        {
+            var books = _bookRepository.GetAllBooks();
+            return Ok(books);
+        }
+    }
+}
---
+++ WebApplication1/Repositories/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Repositories
+{
+    public interface IBookRepository
+    {
+        int AddBook(BookModel book);
+        List<BookModel> GetAllBooks();
+    }
+}
