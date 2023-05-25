23b1a8ca7d217538c42c2f40e3edfb3e08c571a1
ankitt0786 <134185765+ankitt0786@users.noreply.github.com>
2023-05-25 20:41:47 +0530

    Update BookRepository.cs

--- WebApplication1/Repositories/BookRepository.cs
+++ WebApplication1/Repositories/BookRepository.cs
@@ -4,16 +4,8 @@ namespace WebApplication1.Repositories
 {
     public class BookRepository: IBookRepository
     {
-        /// <summary>
-        /// Hold in-memory books data
-        /// </summary>
         private List<BookModel> books = new List<BookModel>();
 
-        /// <summary>
-        /// Add a new book in the books list
-        /// </summary>
-        /// <param name="book"></param>
-        /// <returns>Id of new book</returns>
         public int AddBook(BookModel book)
         {
             book.Id = books.Count + 1; // Create the incremental Id
@@ -21,10 +13,6 @@ namespace WebApplication1.Repositories
             return book.Id;
         }
 
-        /// <summary>
-        /// Gets all books
-        /// </summary>
-        /// <returns>All books</returns>
         public List<BookModel> GetAllBooks()
         {
             return books;
